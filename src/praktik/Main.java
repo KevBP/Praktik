da74132911ad5838bf8810374737306936536d57
kevbp <legauloisfr@gmail.com>
2015-06-05 18:16:24 +0200

    init

---
+++ src/praktik/Main.java
@@ -0,0 +1,23 @@
+package praktik;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Praktik - Manage your search for internship");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+}
