75f82f776de844fac9b0522ad29ea5438957948c
kevbp <legauloisfr@gmail.com>
2015-06-05 18:21:35 +0200

    added Controller class

---
+++ src/praktik/Controller.java
@@ -0,0 +1,23 @@
+package praktik;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+public class Controller {
+    public TableView table;
+
+    public void action_save(ActionEvent actionEvent) {
+    }
+
+    public void action_load(ActionEvent actionEvent) {
+    }
+
+    public void action_about(ActionEvent actionEvent) {
+    }
+
+    public void action_add(ActionEvent actionEvent) {
+    }
+
+    public void action_delete(ActionEvent actionEvent) {
+    }
+}
